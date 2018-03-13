93d750d0abb2b7793d4dcdd1d245ef113197cb86
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-03-13 10:51:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -19,7 +19,6 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-
     case params[:operation]
     when "add"
       (params[:number1].to_i + params[:number2].to_i).to_s
